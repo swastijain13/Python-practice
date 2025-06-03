7cb6d3df28b45c1da416fa71c38ebb69453febdf
Swasti Jain <swasti.j@consultadd.com>
2025-06-03 21:27:36 +0530

    added authentication views

--- db/myproject/myapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.http import JsonResponse
-
-
-def home_view(request):
-    return JsonResponse({"message": "Welcome to My project!!"})
-
--- db/myproject/myapp/views.py
+++ db/myproject/myapp/views.py
@@ -1,3 +1,93 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+
+from db.myproject.myapp.serializers import UserSerializer
+
+
+def home_view(request):
+    return JsonResponse({"message": "Welcome to My project!!"})
+
+@api_view(["POST"])
+def signup(request):
+    try:
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(
+                {
+                    "status": "success",
+                    "message": "User created successfully",
+                    "data": serializer.data,
+                },
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(
+            {"status": "error", "message": serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+    except Exception as e:
+        return Response(
+            {"status": "error", "message": str(e)}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+@api_view(["POST"])
+def login(request):
+    try:
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            return Response(
+                {"status": "error", "message": "Invalid credentials"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        refresh = RefreshToken.for_user(user)
+        refresh["is_admin"] = user.is_admin
+        refresh["username"] = user.username
+
+        response = Response(
+            {
+                "status": "success",
+                "username": user.username,
+                "is_admin" : user.is_admin,
+                "message": "Login successful",
+                "access": str(refresh.access_token),
+                "refresh": str(refresh),
+            },
+            status=status.HTTP_200_OK,
+        )
+
+        return response
+
+    except Exception as e:
+        return Response(
+            {"status": "error", "message": str(e)}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+@api_view(["POST"])
+def logout(request):
+    try:
+        token = request.data["refresh_token"]
+        refresh_token = RefreshToken(token)
+        refresh_token.blacklist()
+
+        return Response(
+            {"status": "success", "message": "Successfully logged out"},
+            status=status.HTTP_200_OK,
+        )
+
+    except Exception as e:
+        return Response(
+            {"status": "error", "message": "Invalid token", "details": str(e)},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
